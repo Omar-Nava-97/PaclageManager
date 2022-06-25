060a2594af41f4abb45342c05881aebe7be44aac
Omar Nava <omar.mac.97@gmail.com>
2022-06-25 11:39:16 -0500

    Initial Commit

---
+++ PaclageManager/PaclageManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaclageManagerApp.swift
+//  PaclageManager
+//
+//  Created by Ricardo Omar Martinez Nava on 25/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PaclageManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
